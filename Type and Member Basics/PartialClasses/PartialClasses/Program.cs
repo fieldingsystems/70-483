9edf6f7099e222fb2c5afb745a86588c6f9518a3
alex <alex.compton@fieldingsystems.com>
2015-04-09 21:35:47 -0400

    added examples

---
+++ Type and Member Basics/PartialClasses/PartialClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClasses
+{
+    public partial class Program
+    {
+        static void Main(string[] args)
+        {
+            // call the partial class
+            OutPut();
+
+            Console.ReadKey();
+        }
+    }
+
+    public partial class Program
+    {
+        public static void OutPut()
+        {
+            Console.WriteLine("I'm a partial Class");
+        }
+    }
+}
---
+++ Type and Member Basics/StaticClasses/StaticClasses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClasses
+{
+    /// <summary>
+    /// 
+    /// static classes exist simply as a way to group  a set of related memers together.
+    /// such as the Math or Console class they have the following restrictions the compiler
+    /// will make the class both abstract and sealed
+    /// 
+    /// * the class must be derived directly from System.Object, you can not instanciate a
+    /// static class
+    /// 
+    /// * the class must not implement any interface, interface methods are callable only
+    /// when using and instance of a class
+    /// 
+    /// * the class must define only static members
+    /// 
+    /// * the class cannot be used as a field, method arameter, or local variable because 
+    /// all of these would indicate a variable that refers to an instance
+    /// 
+    /// </summary>
+    public static class StaticClass
+    {
+        public static void Method()
+        {
+            Console.WriteLine("Static Method");
+        }
+
+        public static String staticProperty
+        {
+            get { return s_staticField; }
+            set { s_staticField = value; }
+        }
+
+        private static string s_staticField;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this was just to show how you can use a static class");
+            Console.ReadKey();
+        }
+    }
+}
